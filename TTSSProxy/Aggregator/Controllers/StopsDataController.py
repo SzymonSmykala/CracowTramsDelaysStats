845a339a5dedb296d9564c026d27d8c6b022ab16
SzymonSmykala <ctk7000@gmail.com>
2020-02-11 06:55:10 +0100

    Added checking if stops exists in python module

--- TTSSProxy/Aggregator/Controllers/StopsDataController.py
+++ TTSSProxy/Aggregator/Controllers/StopsDataController.py
@@ -1,7 +1,27 @@
+import json
+
+import requests
+
+from Aggregator.DTO.Stop import Stop
 from Aggregator.backend_endpoints import BackendEndpoints
 
 
 class StopsDataController(object):
-    def check_if_stop_exists(self, stop_name):
+    @staticmethod
+    def check_if_stop_exists(stop_name):
         url = BackendEndpoints.all_stops_data
-        try
+        response = requests.get(url)
+        objects = json.loads(response.text)
+        list = StopsDataController.deserialize(objects)
+        for stop in list:
+            if stop.stop_name == stop_name:
+                return True
+        return False
+
+    @staticmethod
+    def deserialize(objects):
+        stops = objects["stops"]
+        result = []
+        for obj in stops:
+            result.append(Stop(obj["shortName"], obj["name"]))
+        return result
---
+++ TTSSProxy/Aggregator/DTO/Stop.py
@@ -0,0 +1,8 @@
+class Stop(object):
+    stop_id: str
+    stop_name: str
+    
+    def __init__(self, stop_id, stop_name):
+        self.stop_id = stop_id
+        self.stop_name = stop_name
+
